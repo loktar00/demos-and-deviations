8fc10ead1c203374bdacdefc55818bbced0ac9ae
Jason <loktar69@hotmail.com>
2022-08-20 22:40:59 -0400

    lint fixes, removed index from original location, adjusted build script

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,42 +1,44 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {}
-    }
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    env: {
+        browser: true,
+        es2021: true
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
-  rules: {
-    'linebreak-style': 'off',
-    'comma-dangle': ['error', 'never'],
-    'import/extensions': [
-      'error', {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never'
-      }
+    settings: {
+        'import/resolver': {
+            typescript: {}
+        }
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb'
     ],
-    'react/function-component-definition': [1, { namedComponents: 'arrow-function' }],
-    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
-    'react/jsx-indent': [2, 4]
-  }
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint'
+    ],
+    rules: {
+        'linebreak-style': 'off',
+        'comma-dangle': ['error', 'never'],
+        'import/extensions': [
+            'error', {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never'
+            }
+        ],
+        'arrow-parens': ['error', 'as-needed'],
+        'react/function-component-definition': [1, { namedComponents: 'arrow-function' }],
+        'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+        indent: ['error', 4],
+        'react/jsx-indent': [2, 4]
+    }
 };
--- scripts/makeSite.js
+++ scripts/makeSite.js
@@ -1,9 +1,12 @@
-import list from '../dist/list.json' assert { type: 'json'};
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable import/extensions */
 import path from 'path';
-import { readFile } from "fs/promises";
-import fs from "fs-extra";
-import { writeFileSync } from "fs";
+import { readFile } from 'fs/promises';
+import fs from 'fs-extra';
+import { writeFileSync } from 'fs';
 import { fileURLToPath } from 'url';
+
+import list from '../dist/list.json' assert { type: 'json'};
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,17 +14,13 @@ const __dirname = path.dirname(__filename);
 // Load the index.html file
 async function readIndex() {
     return readFile(path.join(__dirname, './index.html'), 'utf8')
-      .then(data => {
-          return data;
-      });
+        .then(data => data).catch(err => { console.error(err); });
 }
 
 (async () => {
     const fileData = await readIndex();
 
-    const links = list.map(file => {
-        return `<li><a href="demos/${file.name}">${file.name}</a></li>\n`;
-    });
+    const links = list.map(file => `<li><a href="demos/${file.name}">${file.name}</a></li>\n`);
 
     const html = fileData.replace('{{main}}', `<ol>${links.join('')}</ol>`);
 
@@ -29,16 +28,15 @@ async function readIndex() {
 
     try {
         fs.copySync(path.join(__dirname, '../public/'), path.join(__dirname,'../dist/'));
-        console.log('public files copy succes!')
-      } catch (err) {
-        console.error(err)
-      }
+        console.log('public files copy succes!');
+    } catch (err) {
+        console.error(err);
+    }
 
     try {
         fs.copySync(path.join(__dirname, '../demos'), path.join(__dirname,'../dist/demos'));
-        console.log('demo copy succes!')
-      } catch (err) {
-        console.error(err)
-      }
-
+        console.log('demo copy succes!');
+    } catch (err) {
+        console.error(err);
+    }
 })();
